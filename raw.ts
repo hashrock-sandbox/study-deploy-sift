a9682d7b93efb972633e453916017ffa4a49c6d6
hashrock <hashrock@users.noreply.github.com>
2021-09-08 00:46:18 +0900

    add raw static file

---
+++ raw.ts
@@ -0,0 +1,31 @@
+async function handleRequest(request: Request) {
+  const { pathname } = new URL(request.url);
+  if (pathname.startsWith("/style.css")) {
+    const file = await Deno.readFile("./deno.png");
+    return new Response(file, {
+      headers: {
+        "content-type": "image/png",
+      },
+    });
+  }
+
+  return new Response(
+    `<html>
+        <head>
+        </head>
+        <body>
+          <h1>Example</h1>
+          <img src="deno.png">
+        </body>
+      </html>`,
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    }
+  );
+}
+
+addEventListener("fetch", (event: FetchEvent) => {
+  event.respondWith(handleRequest(event.request));
+});
