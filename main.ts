29be00f1c89307dc26b289424d2afaa7bd08c9ed
hashrock <hashrock@users.noreply.github.com>
2021-09-08 00:33:30 +0900

    upd

--- main.ts
+++ main.ts
@@ -1,19 +1,6 @@
-import { h, json, jsx, serve } from "https://deno.land/x/sift/mod.ts";
+import { h, json, serve } from "https://deno.land/x/sift/mod.ts";
 
-const App = () => (
-  <div>
-    <h1>Hello world!</h1>
-  </div>
-);
-
-const NotFound = () => (
-  <div>
-    <h1>Page not found</h1>
-  </div>
-);
 
 serve({
-  "/": () => jsx(<App />),
-  "/api": () => json({ message: "Hello world" }),
-  404: () => jsx(<NotFound />, { status: 404 }),
+  "/": () => json({ message: "Hello world" })
 });
