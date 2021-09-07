d16e975c1b03cf0d7c7246a7cf145ce25000ad88
hashrock <hashrock@users.noreply.github.com>
2021-09-08 00:36:12 +0900

    upd

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import { h, json, serve } from "https://deno.land/x/sift/mod.ts";
-
+import { h, json, serve, serveStatic } from "https://deno.land/x/sift/mod.ts";
 
 serve({
+  "/deno.png": serveStatic("deno.png"),
   "/": () => json({ message: "Hello world" })
 });
